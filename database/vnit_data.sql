8fd3d065549283c90acc53cbebcf9946e9ba8b24
Pratham Giri <pratham.giri02@gmail.com>
2023-08-14 22:07:18 +0530

    Updated database

--- database/vnit_data.sql
+++ database/vnit_data.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 14, 2023 at 06:28 PM
+-- Generation Time: Aug 14, 2023 at 06:34 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
