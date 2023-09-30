dd02da40b4e50acd178657e317ddabef63ca9e2e
DeepSwarup <deepswarup1@gmail.com>
2023-09-30 15:33:16 +0530

    last commit

--- pages/community_page.php
+++ pages/community_page.php
@@ -11,6 +11,7 @@ include "back/database_connection.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../css/circlesprofile.css">
+    <link rel="stylesheet" href="../css/about.css">
 </head>
 <body>
     <?php include 'navbar.php' ?>
@@ -31,9 +32,9 @@ include "back/database_connection.php";
             <div class="threads">
                 <ul>
                     <li> About</li>
-                    <li> About</li>
-                    <li> About</li>
-                    <li> About</li>
+                    <li> Events</li>
+                    <li> This Month Events</li>
+                    <li> Want to contribute</li>
 
                 </ul>
             </div>
@@ -45,19 +46,226 @@ include "back/database_connection.php";
 
         <div class="profile-section">
 
-        <div class="profile">
-            <img src="../images/profile_img/User_icon.png" alt="">
-            <div class="content">
-
-            <h1>Circle Name</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci iusto impedit officia voluptatem quo! Ab blanditiis reiciendis nemo inventore distinctio iste a aperiam? Consequatur nesciunt amet architecto porro enim aperiam?</p>
+            <div class="profile">
+             <img src="../images/profile_img/User_icon.png" alt="">
+                <div class="content">
+                    <h1>Circle Name</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci iusto impedit officia voluptatem quo! Ab blanditiis reiciendis nemo inventore distinctio iste a aperiam? Consequatur nesciunt amet architecto porro enim aperiam?</p>
+                </div>
             </div>
 
-        </div>
+            
+            <div class="card-container">
+            <div class="card">
+            <img src="/collageCommunity/images/profile_img/User_icon.png">
+            <div class="card-content">
+                <h2>Pratham Giri</h2>
+                <p>Co-Founder, CEO</p>
+            </div>
+            </div>
+                <div class="card">
+            <img src="/collageCommunity/images/profile_img/User_icon.png">
+            <div class="card-content">
+                <h2>Deep Swarup</h2>
+                <p>Co-Founder, CTO</p>
+            </div>
+            </div>
+            <div class="card">
+            <img src="/collageCommunity/images/profile_img/User_icon.png">
+            <div class="card-content">
+                <h2>Dhruv Gupta</h2>
+                <p>Co-Founder, COO</p>
+            </div>
+            </div>
+
+
+            <div class="card">
+            <img src="/collageCommunity/images/profile_img/User_icon.png">
+            <div class="card-content">
+                <h2>Pratham Giri</h2>
+                <p>Co-Founder, CEO</p>
+            </div>
+            </div>
+            <div class="card">
+            <img src="/collageCommunity/images/profile_img/User_icon.png">
+            <div class="card-content">
+                <h2>Deep Swarup</h2>
+                <p>Co-Founder, CTO</p>
+            </div>
+            </div>
+            <div class="card">
+            <img src="/collageCommunity/images/profile_img/User_icon.png">
+            <div class="card-content">
+                <h2>Dhruv Gupta</h2>
+                <p>Co-Founder, COO</p>
+            </div>
+            </div>
+             </div>
         
+        </div>
+
+        <div class="events">
+            <h1>
+                Events
+            </h1>
+
+            <div class="card-container">
+            <div class="card">
+            <img src="/collageCommunity/images/profile_img/User_icon.png">
+            <div class="card-content">
+                <h2>Pratham Giri</h2>
+                <p>Co-Founder, CEO</p>
+            </div>
+            </div>
+                <div class="card">
+            <img src="/collageCommunity/images/profile_img/User_icon.png">
+            <div class="card-content">
+                <h2>Deep Swarup</h2>
+                <p>Co-Founder, CTO</p>
+            </div>
+            </div>
+            <div class="card">
+            <img src="/collageCommunity/images/profile_img/User_icon.png">
+            <div class="card-content">
+                <h2>Dhruv Gupta</h2>
+                <p>Co-Founder, COO</p>
+            </div>
+            </div>
+
+
+            <div class="card">
+            <img src="/collageCommunity/images/profile_img/User_icon.png">
+            <div class="card-content">
+                <h2>Pratham Giri</h2>
+                <p>Co-Founder, CEO</p>
+            </div>
+            </div>
+            <div class="card">
+            <img src="/collageCommunity/images/profile_img/User_icon.png">
+            <div class="card-content">
+                <h2>Deep Swarup</h2>
+                <p>Co-Founder, CTO</p>
+            </div>
+            </div>
+            <div class="card">
+            <img src="/collageCommunity/images/profile_img/User_icon.png">
+            <div class="card-content">
+                <h2>Dhruv Gupta</h2>
+                <p>Co-Founder, COO</p>
+            </div>
+            </div>
+             </div>
         </div>
     
-        
+        <div class="month-events">
+            <h1>
+                This Months Events
+            </h1>
+
+            <div class="card-container">
+            <div class="card">
+            <img src="/collageCommunity/images/profile_img/User_icon.png">
+            <div class="card-content">
+                <h2>Pratham Giri</h2>
+                <p>Co-Founder, CEO</p>
+            </div>
+            </div>
+                <div class="card">
+            <img src="/collageCommunity/images/profile_img/User_icon.png">
+            <div class="card-content">
+                <h2>Deep Swarup</h2>
+                <p>Co-Founder, CTO</p>
+            </div>
+            </div>
+            <div class="card">
+            <img src="/collageCommunity/images/profile_img/User_icon.png">
+            <div class="card-content">
+                <h2>Dhruv Gupta</h2>
+                <p>Co-Founder, COO</p>
+            </div>
+            </div>
+
+
+            <div class="card">
+            <img src="/collageCommunity/images/profile_img/User_icon.png">
+            <div class="card-content">
+                <h2>Pratham Giri</h2>
+                <p>Co-Founder, CEO</p>
+            </div>
+            </div>
+            <div class="card">
+            <img src="/collageCommunity/images/profile_img/User_icon.png">
+            <div class="card-content">
+                <h2>Deep Swarup</h2>
+                <p>Co-Founder, CTO</p>
+            </div>
+            </div>
+            <div class="card">
+            <img src="/collageCommunity/images/profile_img/User_icon.png">
+            <div class="card-content">
+                <h2>Dhruv Gupta</h2>
+                <p>Co-Founder, COO</p>
+            </div>
+            </div>
+             </div>
+        </div>
+
+        <div class="register-contri">
+            <form action="back/user_info.php" method="post">
+
+            
+            <div class="clg">
+            <label for="College">College:</label>
+            <input type="text" id="clg" name="college" required>
+            </div>
+
+            <div class="sem">
+                <div class="course">
+                <label for="course">Course</label>
+                <input type="text" id="course" name="course" required>
+                </div>
+                <div class="graduation year">
+                <label for="graduation year">Graduating Year</label>
+                <input type="number" id="gradyr" name="graduation year" min="2015" max="2030" step="1" required>
+            </div>
+
+            <div class="pr">
+                <div class="age">
+                <label for="age">Age</label>
+                <input type="number_format" id="age" name="age" required>
+                </div>
+                <div class="gender">
+                <label for="gender">Gender</label>
+                <select name="gender" id="gender" required>
+                    <option value=""></option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="other">Other</option>
+                    <option valuue="don't prefer to say">don't prefer to say</option>
+                </select>
+            </div>
+
+            <div class="domain">
+            <label for="domain">Domain of Contribution</label>
+            <select name="gender" id="gender" required>
+                    <option value=""></option>
+                    <option value="1">Corporate</option>
+                    <option value="2">Operations</option>
+                    <option value="3">Web Development/Tech</option>
+                    <option valuue="4">Public Relation</option>
+                    <option valuue="5">Research</option>
+                    <option value="6">Product Development/Designing</option>
+                </select>
+            </div>
+
+            <div class="bio">
+                <label for="bio">Bio	&#40;include prior experience if any&#41;</label><br>
+                <textarea id="bio" name="bio" rows="3" cols="50" required></textarea>
+            </div>
+
+            <button type="submit" name="sub-btn">Submit</button>
+        </div>
+    
     </div>
 
     
